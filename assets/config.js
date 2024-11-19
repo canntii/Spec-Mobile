6c93497848df6b77222c7498b9ebc71169e15ec3
canntii <bryandc681@gmail.com>
2024-11-19 13:42:47 -0600

    Modificando dependencias

--- assets/config.js
+++
@@ -1,7 +0,0 @@
-export const firebaseConfig = {
-    apiKey: 'AIzaSyB4NOqYWY-LhTLf92hOuxRz2EAKYCahncY',
-    authDomain: 'userface-b47eb.firebaseapp.com',
-    projectId: 'userface-b47eb',
-    storageBucket: 'userface-b47eb.appspot.com',
-    messagingSenderId: '989466484356'
-};
