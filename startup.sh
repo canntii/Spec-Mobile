58100bcd82b8dedb737db3d83423695428d97000
canntii <bryandc681@gmail.com>
2024-11-19 15:38:01 -0600

    Preparacion del CMake para el deploy

--- startup.sh
+++ startup.sh
@@ -1,5 +1,13 @@
 
 
+
+#Instalar cmake por si no esta instalado
+if ! command -v cmake &> /de/null
+then
+  echo "CMake no encontrado, procediendo con instalacion..."
+  apt-get install -y cmake
+fi
+
 source /home/site/wwwroot/.venv/Scripts/activate
 
 uvicorn.run(app, host="0.0.0.0", port=8000)
