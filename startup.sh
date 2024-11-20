597f0108ed0add9308134b6c83e796202b6b22f2
canntii <bryandc681@gmail.com>
2024-11-19 18:54:27 -0600

    Preparacion del CMake para el deploy

--- startup.sh
+++ startup.sh
@@ -1,15 +1,17 @@
 
 
 
-# Verificar si CMake está instalado, e instalarlo si no lo está
+# Actualizar índices de paquetes e instalar CMake si no está presente
 if ! command -v cmake &> /dev/null
 then
-  echo "CMake no encontrado, procediendo con instalación..."
-  apt-get install -y cmake
+  echo "CMake no encontrado, procediendo con actualización de índices e instalación..."
+  apt-get update -y && apt-get install -y cmake
 else
   echo "CMake ya está instalado."
 fi
 
+# Actualizar pip, setuptools y wheel
+pip install --upgrade pip setuptools wheel
 
 
 
